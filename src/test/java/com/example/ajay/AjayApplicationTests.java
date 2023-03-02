f74bb83fb918d097444a9526e2b3e2878d89d765
Satish <tejassatish@hotmail.com>
2023-03-02 22:53:30 +0530

    A

---
+++ src/test/java/com/example/ajay/AjayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ajay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AjayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
